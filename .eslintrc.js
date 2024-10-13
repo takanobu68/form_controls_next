d7be3e79d6308e4bd141d0fd0b70a25e8e4e5612
Takanobu <61861946+takanobu68@users.noreply.github.com>
2024-10-13 16:41:16 +0900

    eslintを機能させるための設定追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,20 @@
 module.exports = {
-  extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
-}
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "next/core-web-vitals",
+    "next/typescript",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  rules: {
+    "no-const-assign": "error",
+    "@typescript-eslint/explicit-function-return-type": "error",
+  },
+  plugins: ["@typescript-eslint"],
+  root: true,
+};
